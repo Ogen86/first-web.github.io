568ff7882712712efdf3ef7fbbb555353f2b1b19
Ogen86 <ogen86@gmail.com>
2020-01-16 10:54:44 +0100

    first try

--- src/components/diceNumber.jsx
+++ src/components/diceNumber.jsx
@@ -5,13 +5,34 @@ class DiceNumber extends Component {
     number: 1
   };
 
+  handleIncrement = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+
+  handleDecrement = () => {
+    if (this.state.number > 0)
+      return this.setState({ number: this.state.number - 1 });
+  };
+
   render() {
     return (
       <React.Fragment>
         <span className="badge badge-primary m-2">the dice's number :</span>
-        <button className="btn btn-secondary btn-sm m-1"> - </button>
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm m-1"
+        >
+          {" "}
+          -{" "}
+        </button>
         <span className="badge badge-primary m-2"> {this.state.number} </span>
-        <button className="btn btn-secondary btn-sm m-1"> + </button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm m-1"
+        >
+          {" "}
+          +{" "}
+        </button>
       </React.Fragment>
     );
   }
