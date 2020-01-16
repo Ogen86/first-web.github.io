63b320cef6b4825b2f1170f1c3d7ec3ab54ef4f5
Ogen86 <ogen86@gmail.com>
2020-01-16 09:34:41 +0100

    first try

--- src/components/diceNumber.jsx
+++ src/components/diceNumber.jsx
@@ -2,19 +2,16 @@ import React, { Component } from "react";
 
 class DiceNumber extends Component {
   state = {
-    number: 0
+    number: 1
   };
 
   render() {
     return (
       <React.Fragment>
         <span className="badge badge-primary m-2">the dice's number :</span>
-        <Input
-          type={"number"}
-          title={"enter a natural number"}
-          value={this.state.number}
-          placeholder={"enter a natural number"}
-        />
+        <button className="btn btn-secondary btn-sm m-1"> - </button>
+        <span className="badge badge-primary m-2"> {this.state.number} </span>
+        <button className="btn btn-secondary btn-sm m-1"> + </button>
       </React.Fragment>
     );
   }
