857185c704db69c55889d748cd6aa5b58f6791ed
Ogen86 <ogen86@gmail.com>
2020-01-16 12:55:33 +0100

    first try

--- src/components/successRate.jsx
+++ src/components/successRate.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import NumberFormat from "react-number-format";
 
 class SuccessRate extends Component {
   state = {
@@ -12,10 +11,11 @@ class SuccessRate extends Component {
         <span className="badge badge-primary m-2">
           the success rate of the throw :
         </span>
-        <NumberFormat
+        <input
+          type="number"
+          name="success rate"
           value={this.state.rate}
-          displayType={"text"}
-          onValueChange={{ rate: this.state.rate }}
+          onChange={{ rate: this.state.rate }}
         />
       </React.Fragment>
     );
